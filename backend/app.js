ceb57bc9661247393942f125bf02023ebb78a225
reem128-alt <122190881+reem128-alt@users.noreply.github.com>
2025-02-23 01:18:07 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000", 
-      "https://health-care-qw9j.vercel.app/"
+      "https://health-care-qw9j.vercel.app"
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
