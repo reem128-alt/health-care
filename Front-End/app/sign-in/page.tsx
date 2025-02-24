d455c1f7ae6c1a662c402845240c0d9950d05e8e
reem128-alt <122190881+reem128-alt@users.noreply.github.com>
2025-02-24 23:51:12 +0300

    Update page.tsx

--- Front-End/app/sign-in/page.tsx
+++ Front-End/app/sign-in/page.tsx
@@ -4,7 +4,7 @@ export default function SignInPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
-        <div className="text-center">
+        <div className="ml-16">
           <h2 className="mt-6 text-3xl font-extrabold text-gray-900">
             Welcome Back
           </h2>
